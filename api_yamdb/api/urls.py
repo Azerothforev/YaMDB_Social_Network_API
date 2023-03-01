797f41791955af802ac0bcff60b4df74914f41fe
Kirill Svidunovich <Altair21817@gmail.com>
2023-03-02 01:11:07 +0300

    Добавлен UsersViewSet

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .v1.views import AuthSignupViewSet
+from .v1.views import AuthSignupViewSet, AuthToken, UsersViewSet
 
 
 def router_register_func(router, patterns):
@@ -11,9 +11,11 @@ def router_register_func(router, patterns):
 
 v1_router = DefaultRouter()
 v1_router_patterns = [
-    ('auth/signup', AuthSignupViewSet, 'signup')]
+    ('auth/signup', AuthSignupViewSet, 'signup'),
+    ('users', UsersViewSet, 'users')]
 
 router_register_func(router=v1_router, patterns=v1_router_patterns)
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls))]
+    path('v1/', include(v1_router.urls)),
+    path('v1/auth/token/', AuthToken, name='auth')]
