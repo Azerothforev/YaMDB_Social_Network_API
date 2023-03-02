637b3db0271992fb2271261fd4e678acbe1cff7e
Kirill Svidunovich <Altair21817@gmail.com>
2023-03-02 21:53:34 +0300

    Добавил is_superuser, добавил DeleteGetPatchPerm

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -3,4 +3,12 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role in ('admin', 'superuser')
+        return (
+            request.user.is_authenticated and (
+                request.user.role in ('admin', 'superuser')
+                or request.user.is_superuser))
+
+
+class DeleteGetPatchPermission(BaseException):
+    def has_permission(self, request, view):
+        return request.method in ('GET', 'PATCH', 'DELETE')
