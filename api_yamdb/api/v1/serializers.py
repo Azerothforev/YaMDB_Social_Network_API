bb6866bbd50091f98bbdd71075df9988d01133d2
Kirill Svidunovich <Altair21817@gmail.com>
2023-03-02 21:57:10 +0300

    Добавил UsersSerializerAdmin

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -10,8 +10,16 @@ class UserSignUpSerializer(ModelSerializer):
         fields = ('username', 'email')
 
 
-class UsersSerializer(ModelSerializer):
+class UsersSerializerAdmin(ModelSerializer):
+    lookup_field = "username"
 
+    class Meta:
+        model = User
+        fields = (
+            'username', 'email', 'first_name', 'last_name', 'bio', 'role')
+
+
+class UsersSerializer(UsersSerializerAdmin):
     class Meta:
         model = User
         fields = (
